ad9cde000acee4eceaebd3ad7826e5cc922b28c4
Valentino Massimo <shouraigen@gmail.com>
2015-12-30 17:33:02 +0000

    create new template action and route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,33 @@
 class ArticlesController < ApplicationController
 
     def new 
-    @article = Article.new
+        @article = Article.new
     end
 
 
     def create 
     @article = Article.new(article_params)
-        @article.save
-        redirect_to_articles_show(@article)
+    if @article.save
+        flash[:notice] = "Article was successfully created"
+    else
+        render 'new'
     end
+    render plain: params[:article].inspect
+
+    end
+    
+    def show
+    @article = Article.find(article_params[:id])
+    end 
+    
+        
 
 
 private 
 
 def article_params
     params.require(:article).permit(:title, :description)
-    
 end
 end
+
+
