944b326c0eccc8cc6c9ea4dc2eb6e5dbf351268d
Valentino Massimo <shouraigen@gmail.com>
2015-12-31 00:57:36 +0000

    Add partials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+    
     def index
         @articles = Article.all
     end
@@ -8,9 +11,9 @@ class ArticlesController < ApplicationController
     def new 
         @article = Article.new
     end
-def edit
-@article = Article.find(params[:id])
-end
+    
+    def edit
+    end
 
 
     def create
@@ -26,7 +29,6 @@ end
 
     
     def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
     flash[:notice] = "Article was successfully updated"
     redirect_to article_path(@article)
@@ -37,11 +39,9 @@ else
 
     
     def show
-    @article = Article.find(params[:id])
     end
     
     def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
@@ -51,7 +51,12 @@ else
     
 
 private
-def article_params
+
+    def set_article
+    @article = Article.find(params[:id])
+    end
+
+    def article_params
     params.require(:article).permit(:title, :description)
-end
+    end
 end
