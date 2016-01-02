6b31f000c005d1f30946149ecca1c4dbc7ce478d
Valentino Massimo <shouraigen@gmail.com>
2016-01-02 00:17:26 +0000

    update errors _form.html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     def create
     @article = Article.new(article_params)
     if @article.save
-    flash[:notice] = "Article was successfully created"
+    flash[:success] = "Article was successfully created"
     redirect_to article_path(@article)
     
     else
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
     
     def update
     if @article.update(article_params)
-    flash[:notice] = "Article was successfully updated"
+    flash[:success] = "Article was successfully updated"
     redirect_to article_path(@article)
 else
     render 'edit'
@@ -43,7 +43,7 @@ else
     
     def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
     redirect_to articles_path
     end
 
